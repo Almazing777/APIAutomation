1a5dd6bbe5296baaf7ad3b6ea2ad31b2c7a17c94
almas <mendygax@gmail.com>
2020-01-05 00:04:08 -0800

    first get request

--- src/test/java/JIraApi.java
+++ src/test/java/JIraApi.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
+import static io.restassured.RestAssured.given;
+
 
 public class JIraApi {
 
     @Test
     public void makeSureJiraIsUp(){
-        given().
+        given()
+                .when().get("http://localhost:8080/secure/Dashboard.jspa")
+                .then().statusCode(200);
     }
 }
